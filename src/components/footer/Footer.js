10bb9e398b47e674d0adbd7040d88b01ef61eea8
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-07-26 21:57:24 +0200

    Code cleanup.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <Fade bottom duration={1000} distance="5px">
     <div className="footer-div">
-      <p className="footer-text">{emoji("Template by Saad Pasta")}</p>
+      <p className="footer-text">{emoji("Big thanks to Saad Pasta")}</p>
     </div>
     </Fade>
   );
