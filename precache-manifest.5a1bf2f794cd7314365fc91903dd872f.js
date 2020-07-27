6437b6f85315c84562b9a2970930aff1e9a60714
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-07-27 17:24:07 +0200

    Updates

---
+++ precache-manifest.5a1bf2f794cd7314365fc91903dd872f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f710da37ac718a892a081e62c891b299",
+    "url": "/index.html"
+  },
+  {
+    "revision": "566f513c0e357d796d43",
+    "url": "/static/css/3.b499fcd2.chunk.css"
+  },
+  {
+    "revision": "ac2932925d47575ebdb3",
+    "url": "/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "ac38dcc0a7242074e06c",
+    "url": "/static/css/main.69ffce26.chunk.css"
+  },
+  {
+    "revision": "b6119f5b9ce8d60f6e98",
+    "url": "/static/js/2.de275053.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.de275053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566f513c0e357d796d43",
+    "url": "/static/js/3.c0d2bf0c.chunk.js"
+  },
+  {
+    "revision": "ac2932925d47575ebdb3",
+    "url": "/static/js/4.6b2c3409.chunk.js"
+  },
+  {
+    "revision": "ac38dcc0a7242074e06c",
+    "url": "/static/js/main.e6a4335c.chunk.js"
+  },
+  {
+    "revision": "232d443e8049b70d925c",
+    "url": "/static/js/runtime-main.e6dc0c4c.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "17a8612916ba54bfbf0e5ac2c1dd2ba1",
+    "url": "/static/media/ers.17a86129.png"
+  },
+  {
+    "revision": "e239276e8780df793305e60e19f9a9fc",
+    "url": "/static/media/fri.e239276e.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
